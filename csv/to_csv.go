9000179a8c132d4b43e9bb2974dff2e0ed975e5b
lucass-work <lucas.spencer@creditkudos.com>
2020-07-07 19:03:55 +0100

    Add more test data, fix empty rows not being created, make IncludedFields behave more efficiently and be easier to use

--- csv/to_csv.go
+++ csv/to_csv.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/csv"
 	"fmt"
+	"sort"
 	"strconv"
 
 	pbV1 "github.com/golang/protobuf/proto"
@@ -11,11 +12,11 @@ import (
 )
 
 type csvInfo struct {
-	Data          map[string]map[string][]string
-	FieldRequired map[string]bool
+	Data map[string]map[string][]string
+	Rows map[string]int
 }
 
-func V1ProtosToCSV(v1Protos []pbV1.Message, fields []string) (map[string]string, error) {
+func V1ProtosToCSV(v1Protos []pbV1.Message, fields map[string][]string) (map[string]string, error) {
 	v2Protos, err := V1ToV2(v1Protos)
 	if err != nil {
 		return nil, err
@@ -24,39 +25,49 @@ func V1ProtosToCSV(v1Protos []pbV1.Message, fields []string) (map[string]string,
 	return V2ProtosToCSV(v2Protos, fields)
 }
 
-func V2ProtosToCSV(protos []pb.Message, fields []string) (map[string]string, error) {
+func V2ProtosToCSV(protos []pb.Message, fields map[string][]string) (map[string]string, error) {
 	if len(protos) == 0 {
 		return nil, fmt.Errorf("cannot convert empty array of protos")
 	}
 
-	/*
-		Generate a map of each CSV file, each containing a map of the fieldname with an array of row values
-	*/
 	info := csvInfo{
 		make(map[string]map[string][]string),
-		make(map[string]bool),
+		make(map[string]int),
 	}
-	protoHeader(protos[0], &info, fields, "", "", "")
+	populateFieldNames(protos[0], &info, fields, "", "", "")
 	for _, proto := range protos {
-		protoBody(proto, &info, "", "", "", 0)
+		populateBody(proto, &info, "", "", "", 0)
 	}
 
 	// Convert the map into rows
-	csvSlice := make(map[string][][]string)
-	for file, data := range info.Data {
-		csvSlice[file] = make([][]string, len(protos)+1)
-		for field, fieldValues := range data {
-			csvSlice[file][0] = append(csvSlice[file][0], field)
+	csvStrings := make(map[string][][]string)
+	for fileName, fileData := range info.Data {
+		// Length is number of rows + 1 for field names
+		csvStrings[fileName] = make([][]string, info.Rows[fileName]+1)
 
+		// Alpha sort fields, go has intentionally non-deterministic key orderings using #range
+		fields := []string{}
+		for field := range fileData {
+			fields = append(fields, field)
+		}
+		sort.Strings(fields)
+
+		// Populate sorted fields
+		for _, field := range fields {
+			// Populate field name
+			csvStrings[fileName][0] = append(csvStrings[fileName][0], field)
+
+			// Populate field values
+			fieldValues := fileData[field]
 			for i, value := range fieldValues {
-				csvSlice[file][i+1] = append(csvSlice[file][i+1], value)
+				csvStrings[fileName][i+1] = append(csvStrings[fileName][i+1], value)
 			}
 		}
 	}
 
 	//Convert to CSV strings
 	csvs := make(map[string]string)
-	for file, data := range csvSlice {
+	for file, data := range csvStrings {
 		buff := bytes.Buffer{}
 		writer := csv.NewWriter(&buff)
 		writer.WriteAll(data)
@@ -67,7 +78,7 @@ func V2ProtosToCSV(protos []pb.Message, fields []string) (map[string]string, err
 	return csvs, nil
 }
 
-func protoHeader(proto pb.Message, csv *csvInfo, fields []string, parent, parentFile, file string) {
+func populateFieldNames(proto pb.Message, csv *csvInfo, includedFields map[string][]string, parent, parentFile, file string) {
 	pr := proto.ProtoReflect()
 	d := pr.Descriptor()
 	pFields := d.Fields()
@@ -83,43 +94,41 @@ func protoHeader(proto pb.Message, csv *csvInfo, fields []string, parent, parent
 
 	// Add index of parent file if neccessary
 	if parentFile != "" {
-		csv.Data[file][parentFile+".id"] = make([]string, 0)
+		parentField := parentFile + ".id"
+		if stringArrayContains(includedFields[file], parentField) {
+			csv.Data[file][parentField] = make([]string, 0)
+		}
 	}
 
 	for i := 0; i < pFields.Len(); i++ {
 		field := pFields.Get(i)
 		fieldName := string(field.Name())
 
+		// Append parent to name eg account.amount.currency
 		if parent != "" {
 			fieldName = parent + "." + fieldName
 		}
 
-		if !stringArrayContains(fields, fieldName) {
-			csv.FieldRequired[fieldName] = false
+		// Only include specified fields
+		if !stringArrayContains(includedFields[file], fieldName) {
 			continue
 		}
 
-		csv.FieldRequired[fieldName] = true
-
+		// This will only convert message fields else it will panic.
 		if field.Cardinality().GoString() == "Repeated" {
-			repeated := pr.Get(field).List()
-			for j := 0; j < repeated.Len(); j++ {
-				m := repeated.Get(j).Message().Interface()
-
-				protoHeader(m, csv, fields, "", file, "")
-			}
+			repeated := pr.Get(field).List().NewElement().Message().Interface()
+			populateFieldNames(repeated, csv, includedFields, "", file, "")
 			continue
 		}
 		if field.Kind().GoString() == "MessageKind" {
-			protoHeader(pr.Get(field).Message().Interface(), csv, fields, fieldName, "", file)
-		} else if field.Kind().GoString() == "GroupKind" {
+			populateFieldNames(pr.Get(field).Message().Interface(), csv, includedFields, fieldName, "", file)
 		} else {
 			csv.Data[file][fieldName] = make([]string, 0)
 		}
 	}
 }
 
-func protoBody(proto pb.Message, csv *csvInfo, parent, parentType, file string, parentID int) {
+func populateBody(proto pb.Message, csv *csvInfo, parent, parentType, file string, parentID int) {
 	pr := proto.ProtoReflect()
 	d := pr.Descriptor()
 	pFields := d.Fields()
@@ -128,44 +137,66 @@ func protoBody(proto pb.Message, csv *csvInfo, parent, parentType, file string, 
 		file = pName
 	}
 
+	//Determine if the row is empty
+	nonEmpty := false
+
 	// Populate index of parent if neccessary
-	if parentType != "" && csv.FieldRequired[parentType+".id"] {
-		csv.Data[file][parentType+".id"] = append(csv.Data[file][parentType], strconv.Itoa(parentID))
+	if parentType != "" {
+		parentField := parentType + ".id"
+		if csv.Data[file][parentField] != nil {
+			csv.Data[file][parentField] = append(csv.Data[file][parentField], strconv.Itoa(parentID))
+			nonEmpty = true
+		}
 	}
 
 	// Populate remaining fields
 	for i := 0; i < pFields.Len(); i++ {
 		field := pFields.Get(i)
 		fieldName := string(field.Name())
-
 		if parent != "" {
 			fieldName = parent + "." + fieldName
 		}
 
+		// Only populate required fields
+		if csv.Data[file][fieldName] == nil {
+			// If the field is a message or repeated then we check recursively instead
+			if field.Kind().GoString() != "MessageKind" && field.Cardinality().GoString() != "Repeated" {
+				continue
+			}
+		}
+		nonEmpty = true
+
+		// Populate repeated subfields into a seperate CSV
+		// This currently only supports message subfields
 		if field.Cardinality().GoString() == "Repeated" {
 			repeated := pr.Get(field).List()
 			for j := 0; j < repeated.Len(); j++ {
 				m := repeated.Get(j).Message().Interface()
-
-				protoBody(m, csv, "", file, "", len(csv.Data[file][fieldName]))
+				populateBody(m, csv, "", file, "", csv.Rows[file])
 			}
 			continue
 		}
-		// // Only populate desired fields
-		if !csv.FieldRequired[fieldName] {
-			fmt.Println(fieldName)
-			continue
-		}
-		if field.Kind().GoString() == "MessageKind" {
-			protoBody(pr.Get(field).Message().Interface(), csv, fieldName, "", file, 0)
-		} else if field.Kind().GoString() == "GroupKind" {
-		} else {
-			value := pr.Get(field).String()
 
-			csv.Data[file][fieldName] = append(csv.Data[file][fieldName], value)
+		// Populate singular subfield into same CSV
+		if field.Kind().GoString() == "MessageKind" {
+			populateBody(pr.Get(field).Message().Interface(), csv, fieldName, "", file, 0)
+			continue
+		} else {
+			csv.Data[file][fieldName] = append(csv.Data[file][fieldName], pr.Get(field).String())
 		}
 	}
 
+	// Add new row if row non-empty and not a subfield
+	if nonEmpty && parent == "" {
+		csv.Rows[file]++
+
+		// Populate any untouched fields, eg unrelated ids
+		for fieldName, fieldValues := range csv.Data[file] {
+			if len(fieldValues) < csv.Rows[file] {
+				csv.Data[file][fieldName] = append(csv.Data[file][fieldName], "")
+			}
+		}
+	}
 }
 
 /* Convert between V1 and V2 go implementations of protos, while both protos are V3 the go representation
--- csv/to_csv_test.go
+++ csv/to_csv_test.go
@@ -45,11 +45,11 @@ var (
 					}},
 			},
 			J: &test_protos.Minion{
-				Ma: "minion-string",
+				Ma: "minion-string 2",
 				Mb: 7,
 				Mc: nil,
 				Md: &test_protos.Child{
-					Ca: "child-string",
+					Ca: "child-string 2",
 				},
 			},
 		},
@@ -81,16 +81,11 @@ func TestProtoHeaders(t *testing.T) {
 	Convey("All fields created", t, func() {
 		info := csvInfo{
 			make(map[string]map[string][]string),
-			make(map[string]bool),
+			make(map[string]int),
 		}
 
-		protoHeader(v2TestMaster[0], &info, nil, "", "", "")
+		populateFieldNames(v2TestMaster[0], &info, nil, "", "", "")
 		data := info.Data
-		fields := info.FieldRequired
-
-		for _, b := range fields {
-			So(b, ShouldBeTrue)
-		}
 
 		master := data["Master"]
 		minion := data["Minion"]
@@ -108,15 +103,104 @@ func TestProtoHeaders(t *testing.T) {
 		So(minion["md.ca"], ShouldBeEmpty)
 
 		So(child["Minion.id"], ShouldBeEmpty)
-		So(child["Master.id"], ShouldBeNil)
+		So(child["Master.id"], ShouldBeEmpty)
 		So(child["ca"], ShouldBeEmpty)
 	})
 
-	Convey("Certain fields created", t, func() {
+	Convey("Specified fields created", t, func() {
+		info := csvInfo{
+			make(map[string]map[string][]string),
+			make(map[string]int),
+		}
 
+		headerFields := make(map[string][]string)
+		headerFields["Master"] = []string{"a", "b", "c", "j", "i", "j.ma"}
+		headerFields["Minion"] = []string{"ma", "mb", "md"}
+		headerFields["Child"] = []string{"ca"}
+
+		populateFieldNames(v2TestMaster[0], &info, headerFields, "", "", "")
+		data := info.Data
+		master := data["Master"]
+		minion := data["Minion"]
+		child := data["Child"]
+
+		So(master["a"], ShouldBeEmpty)
+		So(master["b"], ShouldBeEmpty)
+		So(master["c"], ShouldBeEmpty)
+		So(master["d"], ShouldBeEmpty)
+
+		// Check subfield
+		So(master["j"], ShouldBeNil)
+		So(master["j.ma"], ShouldBeEmpty)
+
+		// Check repeated fields
+		So(minion["ma"], ShouldBeEmpty)
+		So(minion["mb"], ShouldBeEmpty)
+		So(minion["mc"], ShouldBeNil)
+		So(child["ca"], ShouldBeEmpty)
 	})
 }
 
 func TestProtoBody(t *testing.T) {
+	Convey("All fields created", t, func() {
+		info := csvInfo{
+			make(map[string]map[string][]string),
+			make(map[string]int),
+		}
 
+		populateFieldNames(v2TestMaster[0], &info, nil, "", "", "")
+		populateBody(v2TestMaster[0], &info, "", "", "", 0)
+		populateBody(v2TestMaster[1], &info, "", "", "", 0)
+
+		data := info.Data
+		master := data["Master"]
+		minion := data["Minion"]
+		child := data["Child"]
+
+		So(master["a"][0], ShouldEqual, "string")
+		So(master["a"][1], ShouldEqual, "")
+		So(master["j"], ShouldBeNil)
+		So(master["j.ma"][0], ShouldEqual, "minion-string")
+		So(master["j.ma"][1], ShouldEqual, "minion-string 2")
+		So(master["j.md"], ShouldBeNil)
+		So(master["j.md.ca"][0], ShouldEqual, "child-string")
+		So(master["j.md.ca"][1], ShouldEqual, "child-string 2")
+
+		So(minion["Master.id"][0], ShouldEqual, "1")
+		So(minion["Master.id"][1], ShouldEqual, "1")
+		So(minion["ma"][0], ShouldEqual, "minion-string &2")
+		So(minion["ma"][1], ShouldEqual, "minion-string =3")
+
+		So(child["Master.id"][0], ShouldEqual, "")
+	})
+
+	Convey("Specified fields created", t, func() {
+		info := csvInfo{
+			make(map[string]map[string][]string),
+			make(map[string]int),
+		}
+
+		headerFields := make(map[string][]string)
+		headerFields["Master"] = []string{"a", "b", "c", "j", "i", "j.ma", "j.md", "j.md.ca"}
+		headerFields["Minion"] = []string{"ma", "mb", "md"}
+		headerFields["Child"] = []string{"ca"}
+
+		populateFieldNames(v2TestMaster[0], &info, headerFields, "", "", "")
+		populateBody(v2TestMaster[0], &info, "", "", "", 0)
+		populateBody(v2TestMaster[1], &info, "", "", "", 0)
+
+		data := info.Data
+		master := data["Master"]
+		// minion := data["Minion"]
+		// child := data["Child"]
+
+		So(master["a"][0], ShouldEqual, "string")
+		So(master["a"][1], ShouldEqual, "")
+		So(master["j"], ShouldBeNil)
+		So(master["j.ma"][0], ShouldEqual, "minion-string")
+		So(master["j.ma"][1], ShouldEqual, "minion-string 2")
+		So(master["j.md"], ShouldBeNil)
+		So(master["j.md.ca"][0], ShouldEqual, "child-string")
+		So(master["j.md.ca"][1], ShouldEqual, "child-string 2")
+	})
 }
